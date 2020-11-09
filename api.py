49ec11118b4ced8a1e6ead0929bda9a3b3de9baa
rexlx <rexreedfitzhugh@gmail.com>
2020-11-09 09:26:47 -0600

    added api example

---
+++ api.py
@@ -0,0 +1,25 @@
+import requests
+import json
+
+url = "http://192.168.1.42:3000/send"
+
+my_commands = ["df -h", "uptime", "hostname", "sleep 3", "last"]
+
+command = {
+  "cmd": ";".join(my_commands),
+  "user": "rxlx",
+  "host": "svr",
+  "timeout": "120",
+  "ordered": "false"
+}
+
+headers = {'content-type': 'application/json'}
+
+cmd = json.dumps(command)
+res = requests.post(url, data=cmd, headers=headers)
+
+if res.status_code == 200:
+    print(res.text)
+else:
+    print(f"requst sent, but i got the response: {res.status_code}")
+    print(res.text)
