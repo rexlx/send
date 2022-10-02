8c8217708d636a2fa7c5efacfa9e67aa1729ef0d
rex <rexreedfitzhugh@gmail.com>
2022-10-02 12:47:46 -0500

    Update run.sh

--- api/run.sh
+++ api/run.sh
@@ -2,10 +2,9 @@
 
 export DSN="host=localhost port=5432 user=mothman password=o0913d9*3f381865E3409b-f86b4f&74fb599f5b dbname=send-auth connect_timeout=10"
 export ENV=dev
-export CFG="$HOME/opt/brindle/api/data/config.json"
+export CFG="$HOME/opt/send/api/data/config.json"
 
 clear
-#go run ./src/main
 go build -o vapi ./src/main
 echo compiled!
 ./vapi
