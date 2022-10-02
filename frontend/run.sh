1330e18dc50047be111e247f8bd11dae93c1183b
rex <rexreedfitzhugh@gmail.com>
2022-10-02 12:59:53 -0500

    Update run.sh

--- frontend/run.sh
+++ frontend/run.sh
@@ -1,3 +1,3 @@
-export VUE_APP_API_URL=http://storage.nullferatu.com:8888
-export VUE_APP_IMG_URL=http://storage.nullferatu.com:8888/static/img
+export VUE_APP_API_URL=http://api.domain.com:8888
+export VUE_APP_IMG_URL=http://api.domain.com:8888/static/img
 npm run serve
