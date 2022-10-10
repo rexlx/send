656d90430072c14f43e4a087a9c51cc4a7ba2f49
rexlx <rexreedfitzhugh@gmail.com>
2022-10-09 20:14:47 -0500

    config edit bug fix

--- frontend/src/components/ConfigEdit.vue
+++ frontend/src/components/ConfigEdit.vue
@@ -5,44 +5,44 @@
                 <h1 class="mt-3">configuration details</h1>
                 <hr>
                 <form-tag @editConfig="submitHandler" name="userform" event="editConfig">
-                <text-input v-model="store.config.name"
+                <text-input v-model="config.name"
                             type="text"
                             required="true"
                             label="name"
-                            :value="store.config.name"
+                            :value="config.name"
                             name="name"
                             ></text-input>
-                <text-input v-model="store.config.user"
+                <text-input v-model="config.user"
                             type="text"
                             label="username"
                             required="true"
-                            :value="store.config.user"
+                            :value="config.user"
                             name="username"
                             ></text-input>
-                <text-input v-model="store.config.key"
+                <text-input v-model="config.key"
                             type="text"
                             label="key"
-                            :value="store.config.key"
+                            :value="config.key"
                             name="keyfile"
                             ></text-input>
-                <text-input v-model="store.config.port"
+                <text-input v-model="config.port"
                             type="number"
                             required="true"
                             label="port"
-                            :value="store.config.port"
+                            :value="config.port"
                             name="port"
                             ></text-input>
-                <text-input v-model="store.config.timeout"
+                <text-input v-model="config.timeout"
                             type="number"
                             required="true"
                             label="timeout"
-                            :value="store.config.timeout"
+                            :value="config.timeout"
                             name="timeout"
                             ></text-input>
-                <text-input v-model="store.config.hosts"
+                <text-input v-model="config.hosts"
                             type="text"
                             label="hosts"
-                            :value="store.config.hosts"
+                            :value="config.hosts"
                             name="hosts"
                             ></text-input>
                 <hr>
@@ -64,32 +64,28 @@ import TextInput from './forms/TextInput.vue'
 import notie from 'notie'
 import {store} from "./store"
 import router from "../router/index.js"
-// import { ref } from 'vue'
+import { ref } from 'vue'
 import { useRoute } from 'vue-router'
 
 
 export default {
     setup() {
         const route = useRoute()
+        const config = ref('')
         Rules.hasToken()
-        fetch(process.env.VUE_APP_API_URL + "/admin/configs/get/" + route.params.configId, Rules.requestOptions(""))
+        if (route.params.configId > 0) {
+            fetch(process.env.VUE_APP_API_URL + "/admin/configs/get/" + route.params.configId, Rules.requestOptions(""))
             .then((res) => res.json())
             .then((data) => {
                 if (data.error) {
                     this.$emit('error', data.message);
                 } else {
-                    store.config.value = data;
+                    config.value = data;
                 }
             })
+        }
 
-        return { store }
-    },
-    components: {
-        'form-tag': FormTag,
-        'text-input': TextInput,
-    },
-    methods: {
-        submitHandler() {
+        const saveConfig = async () => {
             const data = {
                 id: parseInt(String(this.$route.params.configId), 10),
                 user: store.config.user,
@@ -102,6 +98,42 @@ export default {
                 fatal: false,
                 ordered: false,
                 name: store.config.name
+            }
+            fetch(`${process.env.VUE_APP_API_URL}/admin/configs/save`, Rules.requestOptions(data))
+            .then((res) => res.json())
+            .then((data) => {
+                if (data.error) {
+                    this.$emit('error', data.message);
+                } else {
+                    this.$emit('success', "changes saved");
+                    router.push("/admin/configs")
+                }
+            })
+            .catch((error) => {
+                this.$emit('error', error);
+            })
+        }
+
+        return { store, saveConfig, config }
+    },
+    components: {
+        'form-tag': FormTag,
+        'text-input': TextInput,
+    },
+    methods: {
+        submitHandler() {
+            const data = {
+                id: parseInt(String(this.$route.params.configId), 10),
+                user: this.config.user,
+                key: this.config.key,
+                logpath: "disabled",
+                hosts: this.config.hosts,
+                command: this.config.command,
+                timeout: this.config.timeout,
+                port: this.config.port,
+                fatal: false,
+                ordered: false,
+                name: this.config.name
             }
             fetch(`${process.env.VUE_APP_API_URL}/admin/configs/save`, Rules.requestOptions(data))
             .then((res) => res.json())
