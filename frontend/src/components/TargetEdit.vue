299e5a7aa8d0038d4492d5d6b7e29eadc718c404
rexlx <rexreedfitzhugh@gmail.com>
2022-10-09 20:15:01 -0500

    adding warning banner

--- frontend/src/components/TargetEdit.vue
+++ frontend/src/components/TargetEdit.vue
@@ -1,16 +1,16 @@
 <template>
     <div class="container">
-        <div class="jumbotron jumbotron-fluid">
+        <div class="row">
+            <div class="col">
+                <h1 class="mt-3">target details</h1>
+                <hr>
+                <div class="jumbotron jumbotron-fluid">
                 <div class="container">
                     <h1 class="display-4 errata">this feature incomplete</h1>
                     <p class="lead">you can add as many <em>targets</em> as you'd like.</p>
                     <p>right now we use a slice of strings for hosts. one day it should be a slice of type targets</p>
                 </div>
             </div>
-        <div class="row">
-            <div class="col">
-                <h1 class="mt-3">target details</h1>
-                <hr>
                 <form-tag @editTarget="submitHandler" name="userform" event="editTarget">
                 <text-input v-model="target.address"
                             type="text"
